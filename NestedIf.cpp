6da63a14e701902baf03b2cbf0286f53f4a47b2d
pichiefox <citrazad@gmail.com>
2026-03-14 14:39:33 +0700

    Status NestedIf

--- NestedIf.cpp
+++ NestedIf.cpp
@@ -5,3 +5,17 @@ float Rata_rata(float a, float b)
 {
     return (a+b)/2;
 }
+
+string Status_lulus(float r)
+{
+    if (r >= 60)
+        if(r > 80)
+        {
+            return "Lulus Cumlaude";
+        }
+        else{
+            return "Lulus Baik";
+        }
+        else
+            return "Gagal";
+}
